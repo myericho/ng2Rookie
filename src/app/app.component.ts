b136d0d314d2babe7b1f6a8b9e7f7bdf0fcb08ba
myericho <myericho.ho@gmail.com>
2016-10-01 12:08:11 +0800

    第2種 抓 enter事件方法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,9 @@ export class AppComponent {
 
   private _title = 'app works!';
   keyword: string = 'test';
-  doSearch($event: KeyboardEvent) {
-       //$event.target 目前事件的物件
-    let inputDom =  $event.target as HTMLInputElement;
+  doSearch($event: KeyboardEvent,keyword:string) {
     if ($event.keyCode == 13) {
-     this.keyword = inputDom.value;
+     this.keyword = keyword;
     }
   }
 
