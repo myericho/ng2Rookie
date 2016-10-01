7e6c042df84a0719a509be8e53110cc94e5916ef
myericho <myericho.ho@gmail.com>
2016-10-01 15:10:01 +0800

    @output應用及事件emit 刪除文章

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input ,Output,EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -7,12 +7,15 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
 
-  @Input()
-  item: any;
+  @Input() item: any;
+  @Output() delete = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  doDelete(item){
+      this.delete.emit(item);
+  }
 }
