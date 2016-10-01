644a974949557d22f255d98774333d76c2229df8
myericho <myericho.ho@gmail.com>
2016-10-01 12:02:12 +0800

    加入event按下enter 再挷定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   private _title = 'app works!';
-  keyword :string = 'test';
-
+  keyword: string = 'test';
+  doSearch($event: KeyboardEvent) {
+       //$event.target 目前事件的物件
+    let inputDom =  $event.target as HTMLInputElement;
+    if ($event.keyCode == 13) {
+     this.keyword = inputDom.value;
+    }
+  }
 
   get title() {
 
